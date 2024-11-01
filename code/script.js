6aa385261756c8270fd0ed711169a68a9e1adef6
Mr Fidal <n00b60d4r@gmail.com>
2024-11-01 23:54:56 +0530

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,9 +1,14 @@
 const urlParams = new URLSearchParams(window.location.search);
+
 const redirectUrl = urlParams.get('redirect');
 const returnUrl = urlParams.get('return');
 const nextUrl = urlParams.get('next');
 const alternativeUrl = urlParams.get('alternative');
 const fallbackUrl = urlParams.get('fallback');
+const destinationUrl = urlParams.get('destination');
+const urlParam = urlParams.get('url');
+const destUrl = urlParams.get('dest');
+const continueUrl = urlParams.get('continue');
 const username = urlParams.get('username');
 
 if (redirectUrl) {
@@ -16,6 +21,14 @@ if (redirectUrl) {
     window.location.href = `${alternativeUrl}?user=${username}`;
 } else if (fallbackUrl) {
     window.location.href = `${fallbackUrl}?user=${username}`;
+} else if (destinationUrl) {
+    window.location.href = `${destinationUrl}?user=${username}`;
+} else if (urlParam) {
+    window.location.href = `${urlParam}?user=${username}`;
+} else if (destUrl) {
+    window.location.href = `${destUrl}?user=${username}`;
+} else if (continueUrl) {
+    window.location.href = `${continueUrl}?user=${username}`;
 } else if (username) {
     document.body.innerHTML += `<h2 style="color: #00ff00;">Hello, ${username}!</h2>`;
 }

e1dc9c18538211c09a98041f0b7f5e62b309e099
Mr Fidal <n00b60d4r@gmail.com>
2024-11-01 22:48:37 +0530

    Update script.js

--- code/script.js
+++ code/script.js
@@ -6,22 +6,16 @@ const alternativeUrl = urlParams.get('alternative');
 const fallbackUrl = urlParams.get('fallback');
 const username = urlParams.get('username');
 
-function encodeUsername(name) {
-    return encodeURIComponent(name);
-}
-
 if (redirectUrl) {
-    window.location.href = `${redirectUrl}?user=${encodeUsername(username)}`;
+    window.location.href = `${redirectUrl}?user=${username}`;
 } else if (returnUrl) {
-    window.location.href = `${returnUrl}?user=${encodeUsername(username)}`;
+    window.location.href = `${returnUrl}?user=${username}`;
 } else if (nextUrl) {
-    window.location.href = `${nextUrl}?user=${encodeUsername(username)}`;
+    window.location.href = `${nextUrl}?user=${username}`;
 } else if (alternativeUrl) {
-    window.location.href = `${alternativeUrl}?user=${encodeUsername(username)}`;
+    window.location.href = `${alternativeUrl}?user=${username}`;
 } else if (fallbackUrl) {
-    window.location.href = `${fallbackUrl}?user=${encodeUsername(username)}`;
+    window.location.href = `${fallbackUrl}?user=${username}`;
 } else if (username) {
-    document.body.innerHTML += `<h2 style="color: #00ff00;">Hello, ${encodeUsername(username)}!</h2>`;
-} else {
-    document.body.innerHTML += '<h2 style="color: #ff0000;">Welcome to our site! Please provide a username.</h2>';
+    document.body.innerHTML += `<h2 style="color: #00ff00;">Hello, ${username}!</h2>`;
 }

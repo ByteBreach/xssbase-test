f79e6f3c421d95b3ee2c22e8b92881318c2e8684
Mr Fidal <n00b60d4r@gmail.com>
2024-11-03 00:31:26 +0530

    Update script.js

--- code/script.js
+++ code/script.js
@@ -1,34 +1,24 @@
 const urlParams = new URLSearchParams(window.location.search);
-
-const redirectUrl = urlParams.get('redirect');
-const returnUrl = urlParams.get('return');
-const nextUrl = urlParams.get('next');
-const alternativeUrl = urlParams.get('alternative');
-const fallbackUrl = urlParams.get('fallback');
-const destinationUrl = urlParams.get('destination');
-const urlParam = urlParams.get('url');
-const destUrl = urlParams.get('dest');
-const continueUrl = urlParams.get('continue');
 const username = urlParams.get('username');
 
+const redirectKeywords = [
+    "redirect", "return", "next", "back", "alternative", "fallback", "destination",
+    "continue", "url", "link", "out", "goto", "target", "forward", "redir", 
+    "customRedirect", "edirect", "irect", "redirect_url", "redir_url"
+];
+
+let redirectUrl = null;
+
+for (const [key, value] of urlParams.entries()) {
+    if (redirectKeywords.some(keyword => key.toLowerCase().includes(keyword))) {
+        redirectUrl = value;
+        break;
+    }
+}
+
 if (redirectUrl) {
+    console.log(`Redirecting to: ${redirectUrl}?user=${username}`);
     window.location.href = `${redirectUrl}?user=${username}`;
-} else if (returnUrl) {
-    window.location.href = `${returnUrl}?user=${username}`;
-} else if (nextUrl) {
-    window.location.href = `${nextUrl}?user=${username}`;
-} else if (alternativeUrl) {
-    window.location.href = `${alternativeUrl}?user=${username}`;
-} else if (fallbackUrl) {
-    window.location.href = `${fallbackUrl}?user=${username}`;
-} else if (destinationUrl) {
-    window.location.href = `${destinationUrl}?user=${username}`;
-} else if (urlParam) {
-    window.location.href = `${urlParam}?user=${username}`;
-} else if (destUrl) {
-    window.location.href = `${destUrl}?user=${username}`;
-} else if (continueUrl) {
-    window.location.href = `${continueUrl}?user=${username}`;
 } else if (username) {
     document.body.innerHTML += `<h2 style="color: #00ff00;">Hello, ${username}!</h2>`;
 }
@@ -38,7 +28,12 @@ if (userInput) {
     document.body.innerHTML += `<p>Your input: ${userInput}</p>`;
 }
 
-const customRedirect = urlParams.get('customRedirect');
-if (customRedirect) {
-    window.location.href = customRedirect;
-}
+const maliciousInput = `<script>alert('XSS Vulnerability Demonstration');</script>`;
+document.body.innerHTML += `<div style="border: 1px solid red; padding: 10px; margin: 10px 0;">
+    <h3>Malicious Input Example:</h3>
+    <p>${maliciousInput}</p>
+</div>`;
+
+console.log("Script executed by mrfidal.");
+console.log("Copyright © 2024 mrfidal. All rights reserved.");
+console.log("For more information, visit: https://mrfidal.in");
